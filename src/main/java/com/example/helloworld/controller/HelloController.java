aa0820f5cfe37633b9d296d8290ae9196047af2a
zhangshunjian <190532117@qq.com>
2022-03-22 16:22:06 +0800

    Initial commit

---
+++ src/main/java/com/example/helloworld/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.helloworld.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+
+    @RequestMapping("hello")
+    public String hello(){
+        return "Hello World!";
+    }
+}
